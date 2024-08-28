a2b5546bb36293cd76e5302d4512f53bbbbfe5e5
david <alu0101479507@ull.edu.es>
2024-08-28 12:20:51 +0100

    project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Header } from './components/Header.jsx'
+import { Card } from './components/Card.jsx'
+
+export function App () {
+  return (
+    <>
+      <Header />
+      <main>
+        <Card />
+      </main>
+    </>
+
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import './Card.css'
+
+export function Card () {
+  return (
+    <div className='card'>
+      <div className='card-options' />
+      <input type='text' />
+    </div>
+  )
+}
